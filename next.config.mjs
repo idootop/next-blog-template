d67f4dc6e166268a8db24d3bcd210729c827d291
WJG <wjg12341234@163.com>
2023-05-31 22:01:46 +0800

    Revert "misc: remove markdown support"
    This reverts commit 4d1fea1a03f0794e680de7909305f3f35bb44ac7.

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,24 @@
+import NextMDX from '@next/mdx';
+import rehypeHighlight from 'rehype-highlight';
+import rehypeKatex from 'rehype-katex';
+import { remarkHeadingId } from 'remark-custom-heading-id';
+import remarkGfm from 'remark-gfm';
+import remarkMath from 'remark-math';
+
+const withMDX = NextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkHeadingId, remarkGfm, remarkMath],
+    rehypePlugins: [rehypeHighlight, rehypeKatex],
+  },
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-  pageExtensions: ['ts', 'tsx'],
+  pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
